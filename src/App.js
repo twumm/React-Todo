a8b22e982b3fa389bc207eec294f5b22d017804b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-29 11:34:49 +0000

    feature: Import and add TodoList as an element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -16,6 +17,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList allTodos={this.state.allTodos} />
       </div>
     );
   }
