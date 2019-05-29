42a52327765099331e70c24afbde32493b331d0f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-29 11:35:33 +0000

    feature: map through todo list and create new todo items

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+export default function TodoList({ allTodos }) {
+  return (
+    <div>
+      {
+        allTodos.length <= 0 ? 
+          <div>What do you want to do?</div>
+        :
+        allTodos.map(todo => <Todo key={todo.id} todo={todo} />)
+      }
+    </div>
+  )
+}
+
